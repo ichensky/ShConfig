c77c21bd097de962f16a4f453ed1295b23e0f960
Ivan Chensky <i.chensky@ameria.de>
2017-09-27 15:57:51 +0300

    Add ShConfig lig with example

---
+++ src/ShConfig/ShConfig.Example/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShConfig.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = "file.sh";
+            var dic = new Dictionary<string,string> {
+                ["name"]="Bob",
+                ["email"]="bob@example.com",
+                ["phone"]="+123456789",
+                ["country"]="Earth"
+            };
+
+            Sh.Write(file, dic);
+
+            var data = Sh.Read("file.sh");
+            var email = data["email"];
+        }
+    }
+}
---
+++ src/ShConfig/ShConfig/Sh.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShConfig
+{
+    public static class Sh
+    {
+        public static Dictionary<string, string> Read(string path)
+        {
+            int i;
+            var s = new Dictionary<string,string>();
+            var list = System.IO.File.ReadAllLines(path);
+            foreach (var item in list)
+            {
+                var str=item.TrimStart();
+                if (str.Length>0&&str.IndexOf('#')!=0)
+                {
+                    for (i = 0; i < str.Length; i++)
+                    {
+                        if (str[i]=='=')
+                        {
+                            break;
+                        }
+                    }
+                    if (i==0)
+                    {
+                        throw new Exception("Not valid format of data.");
+                    }
+                    var key = str.Remove(i);
+                    var value = str.Remove(0,i+1);
+
+                    if (s.ContainsKey(key))
+                    {
+                        s[key] = value;
+                    }
+                    else
+                    {
+                        s.Add(key,value);
+                    }
+                }
+            }
+            return s;
+        }
+        public static void Write(string path, Dictionary<string, string> data) {
+            System.IO.File.WriteAllLines(path, data.Select(x => x.Key + '=' + x.Value));
+        }
+    }
+}
